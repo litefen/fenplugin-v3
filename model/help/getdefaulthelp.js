ba327bc1dcaf27a156fac9d29611a0787d31de85
小粉 <litefen@outlook.com>
2022-09-21 11:35:38 +0800

    delete

--- model/help/getdefaulthelp.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-const _path = process.cwd()
-let pathdef = `${_path}/plugins/xiaoxue-plugin/config/default_config/help/help.yaml`
-let path = `${_path}/plugins/xiaoxue-plugin/config/config/help/help.yaml`
-
-let xiaoxuedefaulthelp = fs.readFileSync(pathdef, 'utf8')
-
-fs.writeFileSync(path, xiaoxuedefaulthelp, 'utf8')
